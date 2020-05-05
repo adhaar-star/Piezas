f524c12e75c510f62bfd5b6e7c42b61b729867e2
adhaar-star <59237202+adhaar-star@users.noreply.github.com>
2020-05-05 16:23:58 -0700

    Fixing bugs in travis ci

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -19,50 +19,50 @@ TEST(PiezasTest, sanityCheck)
 	ASSERT_TRUE(true);
 }
 
-TEST(PiezasTest, drop_correct_peiceX)
+TEST(PiezasTest, dropInitial)
 {
 	Piezas test;
 	ASSERT_EQ(test.dropPiece(0), X);
 }
-/*
+
 Test(PiezasTest, dropNext)
 {
-	Piezas unit_test;
-	ASSERT_EQ(unit_test.dropPiece(0), O);
+	Piezas test;
+	ASSERT_EQ(test.dropPiece(0), O);
 }
 
 Test(PiezasTest, makeColumnfull)
 {
-	Piezas unit_test;
-	ASSERT_EQ(unit_test.dropPiece(0), X);
+	Piezas test;
+	ASSERT_EQ(test.dropPiece(0), X);
 }
 
 Test(PiezasTest, BlankCheck)
 {
-	Piezas unit_test;
-	ASSERT_EQ(unit_test.dropPiece(0), Blank);
+	Piezas test;
+	ASSERT_EQ(test.dropPiece(0), Blank);
 }
 
 Test(PiezasTest, dropOutofBonds)
 {
-	Piezas unit_test;
-	ASSERT_EQ(unit_test.dropPiece(5), Invalid);
+	Piezas test;
+	ASSERT_EQ(test.dropPiece(5), Invalid);
 }
 
 Test(PiezasTest, GameNotOver){
-	Piezas unit_test;
-	ASSERT_EQ(unit_test.gameState(), Invalid);
+	Piezas test;
+	ASSERT_EQ(test.gameState(), Invalid);
 }
 
 Test(PiezasTest, checkWinner){
-	Piezas unit_test;
+	Piezas test;
 	 for(int j=0; j<BOARD_ROWS;j++){
     for(int i=0; i<BOARD_COLS;i++){
 		if(board[i][j] == Blank)
-        unit_test.dropPiece(X);      
+        test.dropPiece(X);      
     }
 	 }
-	 ASSERT_EQ(unit_test.gameState(), X);
+	 ASSERT_EQ(test.gameState(), X);
 	
 }
-*/
+
